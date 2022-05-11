d47c54481f3fef7df5bafd6881a63f0d5898972c
matheus-fernano <matheus.fernando.coder@gmail.com>
2022-05-11 18:46:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function mostreConteudo(e, tema) {
+    // Declare all variables
+    var i, conteudo, btn;
+  
+    // Get all elements with class="tabcontent" and hide them
+    conteudo = document.getElementsByClassName("conteudo");
+    for (i = 0; i < conteudo.length; i++) {
+      conteudo[i].style.display = "none";
+      // conteudo[i].style.border = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    btn = document.getElementsByClassName("btn");
+    for (i = 0; i < btn.length; i++) {
+      btn[i].className = btn[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tema).style.display = "block";
+    e.currentTarget.className += " active";
+  } 
